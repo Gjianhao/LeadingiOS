22112aed666ac62ea52ece86fa1ca5ae68138520
Gjianhao <kevinbina@163.com>
2021-03-21 15:59:30 +0800

    创建几个文件

---
+++ JHChatIM/Modules/JHLaunchManager/JHLaunchManager.swift
@@ -0,0 +1,12 @@
+//
+//  JHLaunchManager.swift
+//  JHChatIM
+//
+//  Created by gjh on 2021/3/21.
+//
+
+import UIKit
+
+class JHLaunchManager: NSObject {
+
+}
---
+++ JHChatIM/Modules/JHSDKManager/JHSDKManager.swift
@@ -0,0 +1,12 @@
+//
+//  JHSDKManager.swift
+//  JHChatIM
+//
+//  Created by gjh on 2021/3/21.
+//
+
+import UIKit
+
+class JHSDKManager: NSObject {
+
+}
